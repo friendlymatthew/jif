629d10132d49c949af48c26085b416bd33412f2d
Matthew Kim <38759997+friendlymatthew@users.noreply.github.com>
2024-10-13 21:05:04 -0400

    Specify absolute path, remove stupid dbg

--- bin/render.rs
+++ bin/render.rs
@@ -5,7 +5,7 @@ use gif::{Decoder, dump_gif};
 use gif::grammar::LogicalScreenDescriptor;
 
 fn main() -> Result<()> {
-    let data = dump_gif("../sample_1.gif")?;
+    let data = dump_gif("/Users/matthew/Desktop/gif/sample_1.gif")?;
     let mut decoder = Decoder::new(data);
     let compressed_gif = decoder.parse()?;
 
@@ -27,7 +27,6 @@ fn main() -> Result<()> {
     let picture = &frames[0];
 
     while window.is_open() {
-        dbg!("wef");
         window.update_with_buffer(picture, canvas_width as usize, canvas_height as usize)?;
     }
 
